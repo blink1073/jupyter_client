a67109e8189e0193569a9bc548361f0773b015a8
Thomas Kluyver <thomas@kluyver.me.uk>
2017-07-26 16:35:52 +0100

    Use entry points to find kernel finders

--- setup.py
+++ setup.py
@@ -63,10 +63,10 @@ setup(
         'console_scripts': [
             'jupyter-kernelspec = jupyter_client.kernelspecapp:KernelSpecApp.launch_instance',
             'jupyter-run = jupyter_client.runapp:RunApp.launch_instance',
-            'jupyter-kernel = jupyter_client.kernelapp:main',
         ],
-        'jupyter_client.kernel_provisioners': [
-            'local-provisioner = jupyter_client.provisioning:LocalProvisioner',
-        ],
+        'jupyter_client.kernel_finders' : [
+            'spec = jupyter_client.discovery:KernelSpecFinder',
+            'pyimport = jupyter_client.discovery:IPykernelFinder',
+        ]
     },
 )
